f9cbf76e593b28c7a3782bf76d439d473b391f03
KashSansanwal <kashish.sansanwal39@gmail.com>
2023-01-13 15:50:53 -0700

    Update script.py

--- LAB1/script.py
+++ LAB1/script.py
@@ -2,6 +2,7 @@ import requests
 
 print(requests.__version__)
 
-print(requests.get('http://google.com/'))
+print(requests.get('http://google.com/').content)
 
-print(requests.get('https://raw.githubusercontent.com/KashSansanwal/CMPUT404-LABS/main/LAB1/script.py').content)
+print("____________________________________________________________________________________________________________________________")
+print(requests.get('https://raw.githubusercontent.com/KashSansanwal/CMPUT404-LABS/main/LAB1/script.py').text)
